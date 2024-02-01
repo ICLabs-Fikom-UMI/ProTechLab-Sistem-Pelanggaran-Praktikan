ecd57f60a225ae64db6bcf616d1cb863309017d5
“nofalabiyyu” <naufal.supriadi02@gmail.com>
2024-02-01 17:05:15 +0800

    update

--- app/view/tatib/index.php
+++ app/view/tatib/index.php
@@ -2,16 +2,16 @@
 <div class="container-fluid">
     <img src="<?=BASEURL; ?>/img/logoFikom.png" alt="" width="300px">
 
-    <div class="flex d-flex justify-content-center mt-5">
+    <div class="flex d-flex justify-content-around mt-5">
         <img src="<?=BASEURL; ?>/img/tatib/tatib.png" alt="" width="400px">
-        <img src="<?=BASEURL; ?>/img/tatib/tatib2.png" alt="" width="500px">
+        <img src="<?=BASEURL; ?>/img/tatib/tatib2.png" alt="" width="400px">
     </div>
 
   
 
 </div>
 <?php } else {
-    // Jika role tidak sesuai, redirect ke halaman login
-    header("Location: " . BASEURL . "/login"); // Sesuaikan dengan path login yang sesuai di aplikasi Anda
+    
+    header("Location: " . BASEURL . "/login"); 
     exit();
 }?>
