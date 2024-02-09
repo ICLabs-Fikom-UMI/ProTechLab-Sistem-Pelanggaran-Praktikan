5b8b27f0e0110e9336b5e067f44b8151313d0067
“nofalabiyyu” <naufal.supriadi02@gmail.com>
2024-02-09 22:48:42 +0800

    fitur edit laporan, script js

--- public/js/script.js
+++ public/js/script.js
@@ -27,4 +27,34 @@ $(function () {
 
     });
 
+    $(".tombolTambahLaporan").on("click", function () {
+        $("#judulmodal").html("Tambah Laporan");
+        $(".modal-body button[type=submit]").html("Tambah Data");
+    });
+    
+    $(".tampilEdit").on("click", function () {
+        $("#judulmodal").html("Edit Laporan"); // Mengubah teks judul menjadi "Edit Laporan"
+        $(".modal-body button[type=submit]").html("Simpan Perubahan"); // Mengubah teks tombol menjadi "Simpan Perubahan"
+        $(".modal-body form").attr("action", 'http://localhost/ProTechLab-Sistem-Pelanggaran-Praktikan/public/menu/ubah');
+    
+        const id = $(this).data("id");  
+        // console.log(id);
+    
+        $.ajax({
+            url: 'http://localhost/ProTechLab-Sistem-Pelanggaran-Praktikan/public/menu/getUbah',
+            data: { id : id },
+            method: "post",
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                $("#nama_frek").val(data.nama_frek);
+                $("#id").val(data.id_frek);
+            }
+        });
+    });
+    
+
+
+   
+
 });
